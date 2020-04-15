9057ed284bac1d4b924a83a7c82f99d4bc4d71da
Clay Harned <charne3@lsu.edu>
2020-04-14 21:46:01 -0500

    fix2

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import MyInfoPageView, VotingReminderPageView, PollingLocationPageView
 
 urlpatterns = [
-    path('VotingReminder', VotingReminderPageView.as_view(), name='VotingReminder'),
+    path('VotingReminder/', VotingReminderPageView.as_view(), name='VotingReminder'),
     path('PollingLocation/', PollingLocationPageView.as_view(), name='PollingLocation'),
     path('', MyInfoPageView.as_view(), name='MyInfo'),
 ]
